7bc0d662a80ccb84bc0ad2c7bfc1a1956fbb2d0d
Amey Narvekar <ameynarvekar01@gmail.com>
2024-03-30 19:58:42 -0400

    Changes in Runner file

--- src/test/java/runner/RunnerTest.java
+++ src/test/java/runner/RunnerTest.java
@@ -8,8 +8,9 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/features", 
 				 glue = { "stepdefinitions", "hooks" },
-				 plugin = {"pretty", "html:target/CucumberReports/CucumberReport.html"},
+				 plugin = {"pretty", "json:target/json/file.json","html:target/CucumberReports/CucumberReport.html"},
 				 publish = true)
-public class TestRunner {
+public class RunnerTest {
 
 }
+	
